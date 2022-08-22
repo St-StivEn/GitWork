e29123ad0ba1a9560191290ee2806efe6a63a665
Stiv En <stiven.sandulyak@gmail.com>
2022-08-22 13:35:02 +0300

    Создал метод по возведению числа в степень

--- GitWork/Program.cs
+++ GitWork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,17 @@ namespace GitWork
 {
     internal class Program
     {
+        public int StepNum(int number,int stepen)
+        {
+            int sum = Convert.ToInt32(Math.Pow(number, stepen));
+            Console.WriteLine($"{number}^{stepen}= {sum}.");
+
+
+            return sum;
+        }
         static void Main(string[] args)
         {
+         
         }
     }
 }
